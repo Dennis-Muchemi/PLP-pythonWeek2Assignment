6d5c7c0ac0edb985b79a835b9a923fe2dd1773c5
Dennis-Muchemi <141043043+Dennis-Muchemi@users.noreply.github.com>
2024-11-17 09:32:28 +0300

    week 2 assignment

---
+++ python week 2 assgnm/assgn.py
@@ -0,0 +1,21 @@
+# a. 
+my_list = []
+
+# b. 
+my_list.extend([10, 20, 30, 40])
+
+# c. 
+my_list.insert(1, 15)
+
+# d. 
+my_list.extend([50, 60, 70])
+
+# e. 
+my_list.pop()
+
+# f. 
+my_list.sort()
+
+# g.
+index_of_30 = my_list.index(30)
+print("Index of 30:", index_of_30)
